069867e27f53226a1dc4fd86f6fc2d983114aa70
Laith-HartiB <72162855+Laith-HartiB@users.noreply.github.com>
2020-10-04 16:07:23 -0400

    Add files via upload

---
+++ NewImage/generateTree.ps1
@@ -0,0 +1,12 @@
+# Uses command 'tree' to create a directory tree of the github repo
+# PowerShell requires certain privileges to run a script locally; make sure
+#   you have privileges set up
+
+# To use this script, make sure that it is in the directory
+#   that you want to START in.
+Write-Host "------------------------------------------------------------"
+Write-Host "Mapping the repo directory as a directory tree."
+Write-Host "All files and folders will be shown, starting in THIS folder"
+Write-Host "Saving the output in a text file named: directoryTree.txt"
+Write-Host "------------------------------------------------------------"
+tree /F > directoryTree.txt
